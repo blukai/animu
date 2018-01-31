92075a8c51a105e3600e325a02003b100d885dc5
blukai <blukai@users.noreply.github.com>
2018-01-31 18:21:23 +0200

    electron parcel react

---
+++ apuri/main/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+const createWindow = () => {
+  win = new BrowserWindow({
+    width: 640,
+    height: 480
+  })
+
+  win.loadURL('http://localhost:8000')
+
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
