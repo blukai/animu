3c94a75bf80f50be7ed8290f3e8885f728cf1123
blukai <blukai@users.noreply.github.com>
2018-02-06 22:26:02 +0200

    styled components are awesome, but there is no reason to use it with mui's jss

--- apuri/renderer/.jest/setup.js
+++ apuri/renderer/.jest/setup.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { configure, shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import 'jest-styled-components'
 
 configure({ adapter: new Adapter() })
 
