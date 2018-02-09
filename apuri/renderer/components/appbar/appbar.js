a576d7df8a3273634ae1160df6fdd7f395a5de18
blukai <blukai@users.noreply.github.com>
2018-02-09 15:53:38 +0200

    .

--- apuri/renderer/components/appbar/appbar.js
+++ apuri/renderer/components/appbar/appbar.js
@@ -7,13 +7,13 @@ import MuiAppbar from 'material-ui/AppBar'
 import History from './history'
 import Search from './search'
 
-const styles = theme => ({
+export const styles = {
   appbarRoot: {
     flexDirection: 'row',
     alignItems: 'center',
     height: 42
   }
-})
+}
 
 @withStyles(styles)
 class Appbar extends Component {
