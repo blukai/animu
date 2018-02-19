45c2ebe35a2869febf46f5ce901c5c887957c0a9
blukai <blukai@users.noreply.github.com>
2018-02-19 17:06:11 +0200

    handle methods, return erros, enable'ish cors

--- bakkuendo/functions/graphql/main.go
+++ bakkuendo/functions/graphql/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -17,24 +18,59 @@ type GraphQLRequest struct {
 	Variables     map[string]interface{} `json:"variables,omitempty"`
 }
 
+type badResponse struct {
+	Error string `json:"error"`
+}
+
+func createBadResponse(err string) string {
+	br, _ := json.Marshal(badResponse{err})
+	return string(br)
+}
+
 func handle(ctx context.Context, req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	response := &events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Headers": "Content-Type,X-Api-Key",
+			"Access-Control-Allow-Methods": "POST,OPTIONS",
+		},
+	}
+
+	if req.HTTPMethod == "OPTIONS" {
+		response.StatusCode = http.StatusOK
+		return response, nil
+	}
+
+	if req.HTTPMethod != "POST" {
+		response.StatusCode = http.StatusMethodNotAllowed
+		return response, nil
+	}
+
 	var payload GraphQLRequest
 	if err := json.Unmarshal([]byte(req.Body), &payload); err != nil {
-		return nil, fmt.Errorf("could not unmarshal body: %v", err)
+		response.Body = createBadResponse(fmt.Sprintf("could not unmarshal body: %v", err))
+		response.StatusCode = http.StatusBadRequest
+		return response, nil
 	}
 
 	schema, err := graphql.New()
 	if err != nil {
-		return nil, fmt.Errorf("could not create a schema: %v", err)
+		response.Body = createBadResponse(fmt.Sprintf("could not create a schema: %v", err))
+		response.StatusCode = http.StatusInternalServerError
+		return response, nil
 	}
 
 	executionResult := schema.Exec(ctx, payload.QueryString, payload.OperationName, payload.Variables)
 	responseBody, err := json.Marshal(executionResult)
 	if err != nil {
-		return nil, fmt.Errorf("could not marshal execution result: %v", err)
+		response.Body = createBadResponse(fmt.Sprintf("could not marshal execution result: %v", err))
+		response.StatusCode = http.StatusInternalServerError
+		return response, nil
 	}
 
-	return &events.APIGatewayProxyResponse{Body: string(responseBody), StatusCode: 200}, nil
+	response.Body = string(responseBody)
+	response.StatusCode = http.StatusOK
+	return response, nil
 }
 
 func main() {
--- bakkuendo/functions/graphql/main_test.go
+++ bakkuendo/functions/graphql/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"net/http"
 	"strings"
 	"testing"
 
@@ -10,13 +11,22 @@ import (
 	"github.com/blukai/animu/bakkuendo/internal/testutils"
 )
 
-func TestMain(t *testing.T) {
+func TestQuery(t *testing.T) {
 	testutils.PrepareAnititlesTestdata(t)
 
 	// ----
 
 	req := GraphQLRequest{
-		QueryString: `{ anititles(afterID: 13600) { id } }`,
+		QueryString: `
+			query($aid: Int!) {
+				anititles(afterID: $aid) {
+					id
+				}
+			 }
+		`,
+		Variables: map[string]interface{}{
+			"aid": 13600,
+		},
 	}
 	body, err := json.Marshal(req)
 	if err != nil {
@@ -24,7 +34,8 @@ func TestMain(t *testing.T) {
 	}
 
 	input := events.APIGatewayProxyRequest{
-		Body: string(body),
+		HTTPMethod: "POST",
+		Body:       string(body),
 	}
 	res, err := handle(context.Background(), input)
 	if err != nil {
@@ -33,5 +44,27 @@ func TestMain(t *testing.T) {
 
 	if !strings.Contains(res.Body, "13691") {
 		t.Fatalf("expected result to contain id of Zokuowarimonogatari - 13691\n%s", res.Body)
+	}
+}
+
+func TestMethods(t *testing.T) {
+	for _, testcase := range []struct {
+		method             string
+		expectedStatusCode int
+	}{
+		{"OPTIONS", http.StatusOK},
+		{"ANY", http.StatusMethodNotAllowed},
+	} {
+		input := events.APIGatewayProxyRequest{
+			HTTPMethod: testcase.method,
+		}
+		res, err := handle(context.Background(), input)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if res.StatusCode != testcase.expectedStatusCode {
+			t.Fatalf("unexpected status code: %d != %d", res.StatusCode, testcase.expectedStatusCode)
+		}
 	}
 }
